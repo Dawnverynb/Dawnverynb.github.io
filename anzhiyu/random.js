2e8256343e746b402c4e1b02d6e4004c7b935d76
Dawnverynb <2201686337@qq.com>
2025-11-29 21:19:53 +0800

    Site updated: 2025-11-29 21:19:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/这是一篇新的博文/","2025/10/07/hello-world/","2025/10/20/AI前端/","2025/10/20/linux/"];function toRandomPost(){
+var posts=["2025/10/20/AI前端/","2025/10/07/hello-world/","2025/10/20/linux/","2025/10/20/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
