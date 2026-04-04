83a72ee730e4133599215cf26c4df6fc830dd1d5
Dawnverynb <2201686337@qq.com>
2026-04-04 16:12:05 +0800

    Site updated: 2026-04-04 16:11:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/AI前端/","2025/10/07/hello-world/","2025/10/20/linux/","2026/03/07/MySQL/","2026/04/04/C++基础入门/","2025/10/20/C++/"];function toRandomPost(){
+var posts=["2025/10/20/AI前端/","2025/10/07/hello-world/","2025/10/20/linux/","2026/03/07/MySQL/","2026/01/04/C++基础入门/","2025/10/20/C++/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
